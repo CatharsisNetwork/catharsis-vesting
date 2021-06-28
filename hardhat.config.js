d9b17110e278eb300113d6d4406257377b8bee92
gfijrb <gfijrb@yandex.ua>
2021-06-29 01:55:30 +0300

    init hardhat project

---
+++ hardhat.config.js
@@ -0,0 +1,49 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  defaultNetwork: "localhost",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    hardhat: {
+    },
+    bscTestnet: {
+      url: "https://eth-mainnet.alchemyapi.io/v2/123abc123abc123abc123abc123abcde",
+      // accounts: [privateKey1, privateKey2, ...]
+    }
+  },
+  solidity: "0.8.0",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./tests",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 20000
+  }
+};
+
