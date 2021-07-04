59b87ba2e380ea983c9e7307fd3d707cfb5182f1
gfijrb <gfijrb@yandex.ua>
2021-07-04 22:44:30 +0300

    go to typescript

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,12 @@
-require("@nomiclabs/hardhat-waffle");
+import { task } from "hardhat/config";
+
+import "@nomiclabs/hardhat-waffle";
+// import "@nomiclabs/hardhat-solhint";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
     console.log(account.address);
@@ -22,14 +25,12 @@ module.exports = {
     localhost: {
       url: "http://127.0.0.1:8545"
     },
-    hardhat: {
-    },
     bscTestnet: {
       url: "https://eth-mainnet.alchemyapi.io/v2/123abc123abc123abc123abc123abcde",
       // accounts: [privateKey1, privateKey2, ...]
     }
   },
-  solidity: "0.8.0",
+  solidity: "0.8.4",
   settings: {
     optimizer: {
       enabled: true,
